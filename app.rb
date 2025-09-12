1a30f3f16edb8a8e3f8fd9238097ec126b3c4d42
rubyst <markovoiq@outlook.com>
2025-09-12 16:27:13 +0300

    Add sqlite3 and get_db method

--- app.rb
+++ app.rb
@@ -2,6 +2,17 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def get_db
+	@db = SQLite3::Database.new 'leprosorium.db'
+	@db.results_as_hash
+	return @db
+end
+
+before do
+	get_db
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
